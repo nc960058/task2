c38f351c3627a79cfb557563b4aa238c5cdbea27
nikhil <nc960058@gmail.com>
2020-05-25 19:24:05 +0530

    1 upload

---
+++ index.php
@@ -0,0 +1,8 @@
+[1;5n<?
+[A[A[B[C[Cphp
+ echp ""[D[D[o[C[Chello"
+?>[A[C[C[C[C[C[C[C[C[C[C[C[C[D;[A[D[D[D[D[D[D[D[D
+
+[A[C
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+[C[C
